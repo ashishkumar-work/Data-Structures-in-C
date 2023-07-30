b1f37699bef513dbb4c03b9429a3bb0232f3489f
ashishkumar-work <139689463+ashishkumar-work@users.noreply.github.com>
2023-07-30 23:16:56 +0530

    Linked Lists program one, initial commit(incomplete)

---
+++ Linked Lists/p1.c
@@ -0,0 +1,118 @@
+//Program of single linked list
+#include<stdio.h>
+#include<stdlib.h>
+void display();
+void count();
+void search();
+struct node
+{
+    int info;
+    struct node *link;
+};
+struct node *create_list(struct node *start);
+struct node *addatbeg(struct node *start, int data);
+struct node *addatend(struct node *start, int data);
+struct node *addafter(struct node *start, int data, int item);
+struct node *addbefore(struct node *start, int data, int item);
+struct node *addatpos(struct node *start, int data, int pos);
+struct node *del(struct node *start, int data);
+struct node *reverse(struct node *start);
+
+int main(){
+    struct node *start=NULL;
+    int choice,data,item,pos;
+    while(1){
+        printf("1.Create List\n");
+        printf("2.Display\n");
+        printf("3.Count \n");
+        printf("4.Search\n");
+        printf("5.Add to empty list / Add at beginning\n");
+        printf("6.Add at end\n");
+        printf("7.Add after node\n");
+        printf("8.Add before node\n");
+        printf("9.Add at position\n");
+        printf("10.Delete\n");
+        printf("11.Reverse\n");
+        printf("12.Ouit\n\n");
+        printf("Enter Your Choice: ");
+        scanf("%d", &choice);
+    }
+
+    switch (choice)
+    {
+    case 1:
+    start = create_list(start);
+    break;
+    
+    case 2:
+    display(start);
+    break;
+
+    case 3:
+    count(start);
+    break;
+
+    case 4:
+    printf("Enter the element to be inserted: ");
+    scanf("%d", &data);
+    search(start, data);
+    break;
+
+    case 5:
+    printf("Enter the elememt to be inserted: ");
+    scanf("%d", &data);
+    start = addatbeg(start, data);
+    break;
+
+    case 6:
+    printf("Enter the element to be instered: ");
+    scanf("%d", &data);
+    start = addatend(start, data);
+
+    case 7:
+    printf("Enter the element to be inserted: ");
+    scanf("%d", &data);
+    printf("Enter the element after which to be insert: ");
+    scanf("%d", &item);
+    start = addafter(start, data, item);
+    break;
+
+    case 8:
+    printf("Enter the element to be inserted: ");
+    scanf("%d", &data);
+    printf("Enter the element before which to insert: ");
+    scanf("%d", &item);
+    start = addbefore(start, data, item);
+    break;
+
+    case 9:
+    printf("Enter the element to be inserted: ");
+    scanf("%d", &data);
+    printf("Enter the position at which to insert: ");
+    scanf("%d", &pos);
+    start = addatpos(start, data, pos);
+    break;
+
+    case 10:
+    printf("Enter the element to be deleted: ");
+    scanf("%d", &data);
+    start = del(start, data);
+    break;
+
+    case 11:
+    start = reverse(start);
+    break;
+
+    case 12:
+    exit(1);
+
+
+    default:
+    printf("Wrong choice !");
+        break;
+    }
+
+
+
+}
+
