5f59b28db24a5d340cae5482b49097a6b25fb761
ashishkumar-work <139689463+ashishkumar-work@users.noreply.github.com>
2023-08-29 00:00:36 +0530

    alot of things are added this time

--- Linked Lists/p1.c
+++ Linked Lists/p1.c
@@ -182,3 +182,83 @@ struct node *addatend(struct node *start, int data){
     tmp->link = NULL;
     return start;
 }//End of addatend()
+
+/*Start of addafter*/
+struct node *addafter(struct node *start, int data, int item){
+    struct node *tmp, *p;
+    p = start;
+    while(p!=NULL){
+        if (p->info == item){
+            tmp = (struct node *)malloc(sizeof(struct node));
+            tmp->info = data;
+            tmp->link = p->link;
+            p->link = tmp;
+            return start;
+        }
+        p = p->link;
+    }
+    printf("%d not present in the the list\n", item);
+    return start;
+}//End of addafter
+
+/*Start of addbefore*/
+struct node *addbefore(struct node *start, int data, int item){
+    struct node *tmp, *p;
+    if (start == NULL){
+        printf("List is empty\n");
+        return start;
+    }//IF THE DATA IS TO BE INSERTED BEFORE THE FIRST NODE
+    
+    if (item == start->info){
+        tmp = (struct node *)malloc(sizeof(struct node));
+        tmp->info = data;
+        tmp->link = tmp;
+        start = tmp;
+        return start;
+    }
+    p = start;
+    while(p->link != NULL){
+        if (p->link->info == item){
+            tmp = (struct node *)malloc(sizeof(struct node));
+            tmp->info = data;
+            tmp->link = p->link;
+            p->link = tmp;
+            return start;
+        }
+        p = p->link;
+    }
+    printf("%d not present in the list\n", item);
+    return start;
+}//End of addbefore()
+
+/*Start of addatpos()*/
+struct node *addatpos(struct node *start, int data, int pos){
+    struct node *tmp, *p;
+    int i;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->info = data;
+    if(pos == 1){
+        tmp->link = start;
+        start = tmp;
+        return start;
+    }
+    p = start;
+    for(i=1; i<pos-1 && p!=NULL; i++){
+        p = p->link;
+    }
+    if(p == NULL){
+        printf("There are less than %d elements\n", pos);
+    }
+        else{
+            tmp->link = p->link;
+            p->link = tmp;
+
+        }
+        return start;
+}//End of addatpos()
+
+
+
+
+
+
